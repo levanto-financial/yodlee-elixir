f0405be6519f9d3223339116aee850b832db012b
Mike North <michael.l.north@gmail.com>
2016-02-09 20:14:43 +0900

    more tests

--- test/auth_test.exs
+++ test/auth_test.exs
@@ -33,4 +33,37 @@ defmodule YodleeAuthTest do
     Yodlee.MockHandler.clear_mock_responses
 
   end
+
+  test "successful user login" do
+    Yodlee.MockHandler.add_mock_response "post", "/authenticate/login", fn _ ->
+      {:ok, %{
+        "userContext": %{
+          "conversationCredentials": %{
+            "sessionToken": "abcd1234"
+          }
+        }
+      }}
+    end
+
+    {:ok, response } = Yodlee.Auth.login("test-cobrand-token", "user", "password")
+    assert response.userContext.conversationCredentials.sessionToken
+    Yodlee.MockHandler.clear_mock_responses
+  end
+
+  test "Failed user login" do
+    Yodlee.MockHandler.add_mock_response "post", "/authenticate/login", fn _ ->
+      {:error, %{
+        "Error": [%{
+          "errorDetail": "Invalid User Credentials"
+        }]
+      }}
+    end
+
+    {:error, errors } = Yodlee.Auth.login("test-cobrand-token", "user", "password")
+
+    assert hd(errors).errorDetail == "Invalid User Credentials"
+    Yodlee.MockHandler.clear_mock_responses
+
+  end
+
 end
