a26ae58a1b5a57c36e501b8f66efd14c02ae97a6
Mike North <michael.l.north@gmail.com>
2016-02-12 13:57:54 +0900

    Account summary

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Yodlee.Mixfile do
 
   def project do
     [app: :yodlee,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.1",
      description: "Yodlee Aggregation API",
      package: package,
