2d3494ed2b38f6e05b810c27789e904a3a169656
Mike North <michael.l.north@gmail.com>
2016-02-17 17:39:08 +0800

    Refresh info

--- lib/yodlee/refresh.ex
+++ lib/yodlee/refresh.ex
@@ -1,8 +1,24 @@
-defmodule Yodlee.Site do
+defmodule Yodlee.Refresh do
   @endpoint "/jsonsdk/Refresh"
 
-  def get_info(cobSessionToken, userSessionToken, site_account_id) do
+  def get_site_info(cobSessionToken, userSessionToken, site_account_id) do
     Yodlee.make_request("post", "#{@endpoint}/getSiteRefreshInfo", %{
+      cobSessionToken: cobSessionToken,
+      userSessionToken: userSessionToken,
+      memSiteAccId: site_account_id
+    })
+  end
+
+  def get_infos(cobSessionToken, userSessionToken, site_account_ids) do
+    Yodlee.make_request("post", "#{@endpoint}/getRefreshInfo1", %{
+      cobSessionToken: cobSessionToken,
+      userSessionToken: userSessionToken,
+      itemIds: site_account_ids
+    })
+  end
+
+  def is_refreshing(cobSessionToken, userSessionToken, site_account_id) do
+    Yodlee.make_request("post", "#{@endpoint}/isItemRefreshing", %{
       cobSessionToken: cobSessionToken,
       userSessionToken: userSessionToken,
       memSiteAccId: site_account_id
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Yodlee.Mixfile do
 
   def project do
     [app: :yodlee,
-     version: "0.0.7",
+     version: "0.0.8",
      elixir: "~> 1.1",
      description: "Yodlee Aggregation API",
      package: package,
