f89612db8200af22abbda58eb51ce0d9bac8eb41
Mike North <michael.l.north@gmail.com>
2016-03-08 15:00:26 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Yodlee.Mixfile do
 
   def project do
     [app: :yodlee,
-     version: "0.0.10",
+     version: "0.1.0",
      elixir: "~> 1.1",
      description: "Yodlee Aggregation API",
      package: package,
