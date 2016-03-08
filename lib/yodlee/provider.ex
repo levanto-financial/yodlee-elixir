a4c131585b36fa7c4263b50c1b43978e080656f3
Mike North <michael.l.north@gmail.com>
2016-03-08 19:47:20 +0800

    Fixed conflicting function signatures in Yodlee.Providers.search

--- lib/yodlee/provider.ex
+++ lib/yodlee/provider.ex
@@ -1,15 +1,11 @@
 defmodule Yodlee.Provider do
 
-  def search(cob_session_token, params, cobrand_name \\ Yodlee.default_cobrand_name) do
-    do_search cob_session_token, params, cobrand_name
-  end
-
-  defp do_search(cob_session_token, %{"name" => name}, cobrand_name) do
+  def search(cob_session_token, name, cobrand_name \\ Yodlee.default_cobrand_name) do
     body = %{"name" => name}
     Yodlee.make_authenticated_request(cob_session_token, "get", "#{cobrand_name}/v1/providers", body)
   end
 
-  defp do_search(cob_session_token, %{"skip" => skip, "top" => top}, cobrand_name) do
+  defp search_by_params(cob_session_token, %{"skip" => skip, "top" => top}, cobrand_name \\ Yodlee.default_cobrand_name) do
     body =  %{"skip" => skip || 0, "top" => top || 500}
     Yodlee.make_authenticated_request(cob_session_token, "get", "#{cobrand_name}/v1/providers", body)
   end
